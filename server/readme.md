c0913b213c5ac11a9667c5911e8f6f721b06d174
DannyBoyNYC <daniel.deverell@gmail.com>
2018-12-22 11:15:07 -0500

    initial commit

---
+++ server/readme.md
@@ -0,0 +1,7 @@
+# Source
+
+`https://www.youtube.com/watch?v=ed8SzALpx1Q`
+
+`http://localhost:4000/graphql`
+
+56:04 - no schema
---
+++ server/schema/schema.js
@@ -0,0 +1,38 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+var books = [
+  { name: 'Gone with the Wind', genre: 'classic', id: '1' },
+  { name: 'Blind Boy', genre: 'fantasy', id: '2' },
+  { name: 'Homeaway', genre: 'sci-fi', id: '3' },
+];
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        args.id;
+        // code to get data from db / other source
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  Query: RootQuery,
+});
